076063504e56f175ac9be0f681bffe7fb60dc675
Ted Sazon <tedsazon@gmail.com>
2018-11-20 23:07:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(-2)
+  array.slice(1)
   return array
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
