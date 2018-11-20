21c1939f85d4fb6a8d2f22ae037d794b21147c98
Ted Sazon <tedsazon@gmail.com>
2018-11-20 23:07:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,6 +38,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length - 1)
-  return array
+  return array.slice(0, array.length - 1)
+
 }
