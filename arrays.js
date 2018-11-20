116bb97238e1671de75b744d81f4736840a50750
Ted Sazon <tedsazon@gmail.com>
2018-11-20 22:43:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,3 +13,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 function addElementToEndOfArray(array, element) {
   return [...array, element]
 }
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
